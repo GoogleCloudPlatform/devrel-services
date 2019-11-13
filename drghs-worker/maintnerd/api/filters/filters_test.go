f28e8e77dbe4b85ea1886cdb7aa7206a233b0cb0
Colin Nelson <colnnelson@google.com>
2019-11-13 13:19:17 -0800

    feat(maintner): add filter support
    Adds proper filtering support based on the Common Expression Language
    (https://github.com/google/cel-spec).
    
    This provides the ability for users of the IssueService API to filter
    the results of their queries based on the "filter" string in
    ListIssuesRequest and ListRepositoriesRequest.

---
+++ drghs-worker/maintnerd/api/filters/filters_test.go
@@ -0,0 +1,216 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package filters
+
+import (
+	"testing"
+
+	drghs_v1 "github.com/GoogleCloudPlatform/devrel-services/drghs/v1"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestFilterIssue(t *testing.T) {
+	tests := []struct {
+		Name    string
+		Issue   drghs_v1.Issue
+		Request drghs_v1.ListIssuesRequest
+		Want    bool
+		WantErr bool
+	}{
+		{
+			Name:    "Empty Filter Passes",
+			Issue:   drghs_v1.Issue{},
+			Request:  drghs_v1.ListIssuesRequest{},
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "Null Pull Request Passes",
+			Issue: drghs_v1.Issue{
+				Name: "foo",
+				IsPr: true,
+			},
+			Request:  drghs_v1.ListIssuesRequest{},
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "True Pull Request Passes",
+			Issue: drghs_v1.Issue{
+				IsPr: true,
+			},
+			Request:  drghs_v1.ListIssuesRequest{ 
+				PullRequestNullable: &drghs_v1.ListIssuesRequest_PullRequest{
+					PullRequest: true,
+				},
+			},
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "False Pull Request Passes",
+			Issue: drghs_v1.Issue{
+				IsPr: true,
+			},
+			Request:  drghs_v1.ListIssuesRequest{ 
+				PullRequestNullable: &drghs_v1.ListIssuesRequest_PullRequest{
+					PullRequest: false,
+				},
+			},
+			Want:    false,
+			WantErr: false,
+		},
+		{
+			Name: "True Closed Passes",
+			Issue: drghs_v1.Issue{
+				Closed: true,
+			},
+			Request:  drghs_v1.ListIssuesRequest{ 
+				ClosedNullable: &drghs_v1.ListIssuesRequest_Closed{
+					Closed: true,
+				},
+			},
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "False Closed Passes",
+			Issue: drghs_v1.Issue{
+				Closed: true,
+			},
+			Request:  drghs_v1.ListIssuesRequest{ 
+				ClosedNullable: &drghs_v1.ListIssuesRequest_Closed{
+					Closed: false,
+				},
+			},
+			Want:    false,
+			WantErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		got, goterr := FilterIssue(&test.Issue, &test.Request)
+		if (test.WantErr && goterr == nil) || (!test.WantErr && goterr != nil) {
+			t.Errorf("test: %v, errors diff. WantErr: %v, GotErr: %v.", test.Name, test.WantErr, goterr)
+		}
+		if diff := cmp.Diff(test.Want, got); diff != "" {
+			t.Errorf("test: %v, values diff. match (-want +got)\n%s", test.Name, diff)
+		}
+	}
+}
+
+func TestFilterRepo(t *testing.T) {
+	tests := []struct {
+		Name    string
+		Repo    drghs_v1.Repository
+		Filter  string
+		Want    bool
+		WantErr bool
+	}{
+		{
+			Name:    "Empty Filter Passes",
+			Repo:    drghs_v1.Repository{},
+			Filter:  "",
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "Filter Name Passes",
+			Repo: drghs_v1.Repository{
+				Name: "foo",
+			},
+			Filter:  "repository.name == 'foo'",
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "Wrong Name Fails",
+			Repo: drghs_v1.Repository{
+				Name: "foo",
+			},
+			Filter:  "repository.name == 'bar'",
+			Want:    false,
+			WantErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		got, goterr := FilterRepository(&test.Repo, test.Filter)
+		if (test.WantErr && goterr == nil) || (!test.WantErr && goterr != nil) {
+			t.Errorf("test: %v, errors diff. WantErr: %v, GotErr: %v.", test.Name, test.WantErr, goterr)
+		}
+		if diff := cmp.Diff(test.Want, got); diff != "" {
+			t.Errorf("test: %v, values diff. match (-want +got)\n%s", test.Name, diff)
+		}
+	}
+}
+
+func TestFilterComment(t *testing.T) {
+	tests := []struct {
+		Name    string
+		Comment drghs_v1.GitHubComment
+		Filter  string
+		Want    bool
+		WantErr bool
+	}{
+		{
+			Name:    "Empty Filter Passes",
+			Comment: drghs_v1.GitHubComment{},
+			Filter:  "",
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "Filter Body Passes",
+			Comment: drghs_v1.GitHubComment{
+				Body: "foo",
+			},
+			Filter:  "comment.body == 'foo'",
+			Want:    true,
+			WantErr: false,
+		},
+		{
+			Name: "Wrong Body Fails",
+			Comment: drghs_v1.GitHubComment{
+				Body: "foo",
+			},
+			Filter:  "comment.body == 'bar'",
+			Want:    false,
+			WantErr: false,
+		},
+		{
+			Name: "Deep check passes",
+			Comment: drghs_v1.GitHubComment{
+				User: &drghs_v1.GitHubUser{
+					Login: "foo",
+				},
+			},
+			Filter:  "comment.user.login == 'foo'",
+			Want:    true,
+			WantErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		got, goterr := FilterComment(&test.Comment, test.Filter)
+		if (test.WantErr && goterr == nil) || (!test.WantErr && goterr != nil) {
+			t.Errorf("test: %v, errors diff. WantErr: %v, GotErr: %v.", test.Name, test.WantErr, goterr)
+		}
+		if diff := cmp.Diff(test.Want, got); diff != "" {
+			t.Errorf("test: %v, values diff. match (-want +got)\n%s", test.Name, diff)
+		}
+	}
+}
--- drghs-worker/maintnerd/api/v1beta1/issue_service.go
+++ drghs-worker/maintnerd/api/v1beta1/issue_service.go
@@ -21,13 +21,10 @@ import (
 
 	drghs_v1 "github.com/GoogleCloudPlatform/devrel-services/drghs/v1"
 
+	"github.com/GoogleCloudPlatform/devrel-services/drghs-worker/maintnerd/api/filters"
 	"github.com/GoogleCloudPlatform/devrel-services/drghs-worker/pkg/googlers"
 
 	"golang.org/x/build/maintner"
-
-	"github.com/google/cel-go/cel"
-	"github.com/google/cel-go/checker/decls"
-	"github.com/google/cel-go/common/types"
 
 	"google.golang.org/grpc/codes"
 	healthpb "google.golang.org/grpc/health/grpc_health_v1"
@@ -55,15 +52,15 @@ func NewIssueServiceV1(corpus *maintner.Corpus, resolver googlers.GooglersResolv
 func (s *issueServiceV1) ListRepositories(ctx context.Context, r *drghs_v1.ListRepositoriesRequest) (*drghs_v1.ListRepositoriesResponse, error) {
 	resp := drghs_v1.ListRepositoriesResponse{}
 	err := s.corpus.GitHub().ForeachRepo(func(repo *maintner.GitHubRepo) error {
-		should, err := shouldAddRepository(repo.ID(), r.Filter)
+		rpb, err := makeRepoPB(repo)
+		if err != nil {
+			return err
+		}
+		should, err := filters.FilterRepository(rpb, r.Filter)
 		if err != nil {
 			return err
 		}
 		if should {
-			rpb, err := makeRepoPB(repo)
-			if err != nil {
-				return err
-			}
 			resp.Repositories = append(resp.Repositories, rpb)
 		}
 		return nil
@@ -83,16 +80,17 @@ func (s *issueServiceV1) ListIssues(ctx context.Context, r *drghs_v1.ListIssuesR
 		}
 
 		return repo.ForeachIssue(func(issue *maintner.GitHubIssue) error {
-			should, err := shouldAddIssue(issue, r)
+			iss, err := makeIssuePB(issue, repo.ID(), r.Comments, r.Reviews)
+			if err != nil {
+				return err
+			}
+
+			should, err := filters.FilterIssue(iss, r)
 			if err != nil {
 				return err
 			}
 			if should {
 				// Add
-				iss, err := makeIssuePB(issue, repo.ID(), r.Comments, r.Reviews)
-				if err != nil {
-					return err
-				}
 				resp.Issues = append(resp.Issues, iss)
 			}
 			return nil
@@ -137,81 +135,10 @@ func (s *issueServiceV1) Watch(req *healthpb.HealthCheckRequest, ws healthpb.Hea
 	return status.Errorf(codes.Unimplemented, "health check via Watch not implemented")
 }
 
-func shouldAddIssue(issue *maintner.GitHubIssue, r *drghs_v1.ListIssuesRequest) (bool, error) {
-	if issue.NotExist {
-		return false, nil
-	}
-
-	switch x := r.PullRequestNullable.(type) {
-	case *drghs_v1.ListIssuesRequest_PullRequest:
-		if issue.PullRequest != x.PullRequest {
-			return false, nil
-		}
-	case nil:
-		// Do nothing
-	default:
-		// Do nothing
-	}
-
-	switch x := r.ClosedNullable.(type) {
-	case *drghs_v1.ListIssuesRequest_Closed:
-		if issue.Closed != x.Closed {
-			return false, nil
-		}
-	case nil:
-		// Do nothing
-	default:
-		// Do nothing
-	}
-
-	return true, nil
-}
-
 func getRepoPath(ta *maintner.GitHubRepo) string {
 	return fmt.Sprintf("%v/%v", ta.ID().Owner, ta.ID().Repo)
 }
 
 func getIssueName(ta *maintner.GitHubRepo, iss *maintner.GitHubIssue) string {
 	return fmt.Sprintf("%v/%v/issues/%v", ta.ID().Owner, ta.ID().Repo, iss.Number)
-}
-
-// TODO(orthros) This should default to using *maintner.GitHubRepo, but
-// due to how maintner stores values, this is impossible to mock for tests
-// If other traits of a repository need to be added (labels, milestones etc)
-// in order to support filtering, this funciton signature must be changed
-// e.g. `owner == 'foo' && labels.size() > 10` to find repositories whose
-// owner is 'foo' and number of labels is > 10. This would need to be expanded
-func shouldAddRepository(repoID maintner.GitHubRepoID, filter string) (bool, error) {
-	if filter == "" {
-		filter = defaultFilter
-	}
-
-	env, err := cel.NewEnv(
-		cel.Declarations(
-			decls.NewIdent("repo", decls.String, nil),
-			decls.NewIdent("owner", decls.String, nil)))
-
-	parsed, issues := env.Parse(filter)
-	if issues != nil && issues.Err() != nil {
-		return false, issues.Err()
-	}
-	checked, issues := env.Check(parsed)
-	if issues != nil && issues.Err() != nil {
-		return false, issues.Err()
-	}
-	prg, err := env.Program(checked)
-	if err != nil {
-		return false, err
-	}
-
-	// The `out` var contains the output of a successful evaluation.
-	// The `details' var would contain intermediate evalaution state if enabled as
-	// a cel.ProgramOption. This can be useful for visualizing how the `out` value
-	// was arrive at.
-	out, _, err := prg.Eval(map[string]interface{}{
-		"repo":  repoID.Repo,
-		"owner": repoID.Owner,
-	})
-
-	return out == types.True, nil
 }
