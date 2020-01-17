70270bcc689dc006c5797921b7e466625ccbcfab
Colin Nelson <colnnelson@google.com>
2020-01-17 10:21:31 -0800

    feat(terraform): add backend with future plans in comments

---
+++ .tf/backend.tf
@@ -0,0 +1,9 @@
+# TODO: this file should be uncommented and allow for variable substitution via
+# command line like so
+# https://github.com/hashicorp/terraform/issues/13022#issuecomment-5526106941
+#terraform {
+#  backend "gcs" {
+#  prefix = "terraform/state"
+#  }
+#}
+
