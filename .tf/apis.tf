4985562bbcfa2e5d1cf1d6dd7adaa5f42caa5923
Colin Nelson <colnnelson@google.com>
2020-01-17 10:12:38 -0800

    feat(terraform): add terraform config for apis and variables

---
+++ .tf/apis.tf
@@ -0,0 +1,44 @@
+# Terraform module to enable GCP services in the specified project
+
+resource "google_project_service" "services" {
+  project            = var.project_id
+  disable_on_destroy = false
+
+  for_each = toset([
+    "bigquery.googleapis.com",
+    "bigquerystorage.googleapis.com",
+    "cloudapis.googleapis.com",
+    "cloudbuild.googleapis.com",
+    "clouddebugger.googleapis.com",
+    "clouderrorreporting.googleapis.com",
+    "cloudfunctions.googleapis.com",
+    "cloudkms.googleapis.com",
+    "cloudscheduler.googleapis.com",
+    "cloudtrace.googleapis.com",
+    "compute.googleapis.com",
+    "computescanning.googleapis.com",
+    "container.googleapis.com",
+    "containeranalysis.googleapis.com",
+    "containerregistry.googleapis.com",
+    "containerscanning.googleapis.com",
+    "datastore.googleapis.com",
+    "deploymentmanager.googleapis.com",
+    "endpoints.googleapis.com",
+    "endpointsportal.googleapis.com",
+    "logging.googleapis.com",
+    "monitoring.googleapis.com",
+    "oslogin.googleapis.com",
+    "pubsub.googleapis.com",
+    "replicapool.googleapis.com",
+    "replicapoolupdater.googleapis.com",
+    "resourceviews.googleapis.com",
+    "servicecontrol.googleapis.com",
+    "servicemanagement.googleapis.com",
+    "sql-component.googleapis.com",
+    "stackdriver.googleapis.com",
+    "storage-api.googleapis.com",
+    "storage-component.googleapis.com",
+  ])
+  service = each.key
+}
+
---
+++ .tf/variables.tf
@@ -0,0 +1,25 @@
+# Variables for DRGHS
+
+variable "project_id" {
+  description = "The GCP project ID for this module."
+}
+
+variable "project_name" {
+  description = "The GCP project name for this module, *might* be the same as project_id."
+}
+
+variable "region" {
+  description = "The GCP region for this module."
+}
+
+variable "folder_id" {
+  description = "The Folder the GCP Project is stored in."
+}
+
+variable "billing_account" {
+  description = "The Billing Account associated with the project"
+}
+
+variable "maintner_bucket_name" {
+  description = "Name of the GCS bucket to store Maintner logs to"
+}
