a672092b16ad4f0a43505cb4d30d957f82b47afe
Colin Nelson <colnnelson@google.com>
2020-02-06 10:11:04 -0800

    feat(terraform): enable maintner grpc service

--- .tf/main.tf
+++ .tf/main.tf
@@ -72,34 +72,32 @@ data "google_compute_global_address" "maintner_address" {
   ]
 }
 
-# TODO: Uncomment this when maintner gets the gRPC service
-#
-#resource "google_endpoints_service" "maintner_grpc_service" {
-#  service_name         = "drghs.endpoints.${var.project_id}.cloud.goog"
-#  grpc_config          = <<-EOT
-#  type: google.api.Service
-#  config_version: 3
-#
-#  name: samplr.endpoints.${var.project_id}.cloud.goog
-#  title: DevRel GitHub Services API (TYPE)
-#
-#  apis:
-#  - name: drghs.v1.IssueService
-#
-#  endpoints:
-#  - name: drghs.endpoints.${var.project_id}.cloud.goog
-#    target: "${data.google_compute_global_address.samplr_address.address}"
-#  EOT
-#  protoc_output_base64 = filebase64("../drghs/v1/api_descriptor.pb")
-#
-#  depends_on = [
-#    data.google_compute_global_address.maintner_ip,
-#  ]
-#
-#  lifecycle {
-#    prevent_destroy = true
-#  }
-#}
+resource "google_endpoints_service" "maintner_grpc_service" {
+  service_name         = "drghs.endpoints.${var.project_id}.cloud.goog"
+  grpc_config          = <<-EOT
+  type: google.api.Service
+  config_version: 3
+
+  name: samplr.endpoints.${var.project_id}.cloud.goog
+  title: DevRel GitHub Services API (TYPE)
+
+  apis:
+  - name: drghs.v1.IssueService
+
+  endpoints:
+  - name: drghs.endpoints.${var.project_id}.cloud.goog
+    target: "${data.google_compute_global_address.maintner_address.address}"
+  EOT
+  protoc_output_base64 = filebase64("../drghs/v1/api_descriptor.pb")
+
+  depends_on = [
+    data.google_compute_global_address.maintner_address,
+  ]
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
 #
 # Magic GitHub Proxy Endpoints Configuration
