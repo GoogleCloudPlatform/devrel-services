9e710091d20e407d6f02935f4197af3076c09087
Colin Nelson <colnnelson@google.com>
2020-08-28 14:01:29 -0700

    chore(maintner): update tests for calculateHost

--- drghs-worker/maintner-rtr/main_test.go
+++ drghs-worker/maintner-rtr/main_test.go
@@ -21,7 +21,7 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestCalculateHost(t *testing.T) {
+func TestbuildTR(t *testing.T) {
 	tests := []struct {
 		Name  string
 		Input string
@@ -85,6 +85,85 @@ func TestCalculateHost(t *testing.T) {
 		got := buildTR(c.Input)
 		if diff := cmp.Diff(c.Want, got); diff != "" {
 			t.Errorf("test: %v, values diff. match (-want +got)\n%s", c.Name, diff)
+		}
+	}
+}
+
+func TestCalculateHost(t *testing.T) {
+	tests := []struct {
+		Name    string
+		Input   *repos.TrackedRepository
+		Want    string
+		WantErr error
+	}{
+		{
+			Name: "Success",
+			Input: &repos.TrackedRepository{
+				Owner: "foo",
+				Name:  "bar",
+			},
+			Want:    "mtr-s-9bec779ac30c3c91e5e8055c0f5cb25f39ed7ce0ecb5f9a8f64fdab0:80",
+			WantErr: nil,
+		},
+		{
+			Name: "Handles '.' in repo",
+			Input: &repos.TrackedRepository{
+				Owner: "foo",
+				Name:  "bar.io",
+			},
+			Want:    "mtr-s-cf8092de84b0aecbc785168376cc27fe92962329a7ce274b99e6b867:80",
+			WantErr: nil,
+		},
+		{
+			Name: "Handles '.' in owner",
+			Input: &repos.TrackedRepository{
+				Owner: "foo.com",
+				Name:  "bar",
+			},
+			Want:    "mtr-s-44610f96da7a230fc7b41ee569ca88d47ff9cf2c723c936ca7a84014:80",
+			WantErr: nil,
+		},
+		{
+			Name: "Handles '.' in both",
+			Input: &repos.TrackedRepository{
+				Owner: "foo.com",
+				Name:  "bar.io",
+			},
+			Want:    "mtr-s-643d239d7e9b88b103a918eb67af7f276a1020538b101cc4bcbd0c1b:80",
+			WantErr: nil,
+		},
+		{
+			Name: "Handles ':'",
+			Input: &repos.TrackedRepository{
+				Owner: "foo",
+				Name:  "foo:bar",
+			},
+			Want:    "mtr-s-98260423ee830fd047446b610c3ea2a5e0246de54e0c3ddc49e5e9f7:80",
+			WantErr: nil,
+		},
+		{
+			Name: "Handles deep names",
+			Input: &repos.TrackedRepository{
+				Owner: "foo",
+				Name:  "foo:bar",
+			},
+			Want:    "mtr-s-98260423ee830fd047446b610c3ea2a5e0246de54e0c3ddc49e5e9f7:80",
+			WantErr: nil,
+		},
+		{
+			Name:    "Handles invalid",
+			Input:   nil,
+			Want:    devnull,
+			WantErr: nil,
+		},
+	}
+	for _, c := range tests {
+		got, gotErr := calculateHost(c.Input)
+		if gotErr != c.WantErr {
+			t.Errorf("%v Errors Differ. Want %v. Got %v", c.Name, c.WantErr, gotErr)
+		}
+		if c.Want != got {
+			t.Errorf("%v Outputs Differ. Want %v. Got %v", c.Name, c.Want, got)
 		}
 	}
 }
