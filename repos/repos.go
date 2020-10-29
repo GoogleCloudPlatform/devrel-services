6a23a2790fd126a31ea2a0680df7cce732df14a1
Colin Nelson <colnnelson@google.com>
2020-10-29 09:19:22 -0700

    fix(repos): fixed seralization of default_branch

--- repos/repos.go
+++ repos/repos.go
@@ -32,7 +32,7 @@ type RepoList interface {
 type TrackedRepository struct {
 	Owner              string `json:"owner"`
 	Name               string `json:"name"`
-	DefaultBranch      string `json:"default_branch"`
+	DefaultBranch      string `json:"defaultBranch"`
 	IsTrackingIssues   bool   `json:"isTrackingIssues"`
 	IsTrackingSnippets bool   `json:"isTrackingSnippets"`
 }
